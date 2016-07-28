57cfffc2eac984ece57ec3a488df77da27d4f1e8
ikebets <ilia.kebets@epfl.ch>
2016-07-28 17:27:38 +0200

    implement copy data test

---
+++ test/acceptance/copy-data.test.js
@@ -0,0 +1,46 @@
+/* global describe, it, before, after */
+
+var pyUtils = require('../../source/main');
+
+describe('copyData', function () {
+  this.timeout(10000);
+
+  it('must copy the data from an account in another if the destination stream exists and the' +
+    ' tokens for both accounts are valid and have the appropriate rights', function (done) {
+    var sourceConnection = new pryv.Connection({
+        username: 'javalibtest',
+        auth: 'ciqnpjz9u0gly91pny34szs2e',
+        domain: 'pryv.me'
+      }),
+      destinationConnection = new pryv.Connection({
+        username: 'javalibtest2',
+        auth: 'cir6aw6yx504zzqyq1zpaysig',
+        domain: 'pryv.me'
+      });
+
+    var params = {
+      sourceConnection: sourceConnection,
+      destinationConnection: destinationConnection,
+      sourceStreamId: 'test',
+      destinationStreamId: 'copy-data-stream',
+      getEventsFilter: {
+        limit: 1000
+      },
+      filtering: 10
+    };
+
+
+    pyUtils.copyData(params, function (err, res) {
+      if (err) {
+        return done(err);
+
+      }
+      res.forEach(function (r) {
+        console.log(r);
+      })
+      done(err);
+    });
+  });
+
+
+});
