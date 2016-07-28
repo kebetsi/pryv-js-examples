a8451b86cc7c484099941fd1e83cc49f24e37b8b
ikebets <ilia.kebets@epfl.ch>
2016-07-28 16:59:17 +0200

    add entry

---
+++ source/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  copyData: require('./methods/copy-data.js'),
+  createNumericEvents: require('./methods/create-numeric-events.js'),
+  findStreamNames: require('./methods/find-stream-ids.js')
+};
